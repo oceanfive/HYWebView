592f77ae26058d50889b7d4687bd881b6eca2900
oceanfive <849638313@qq.com>
2017-09-12 10:29:45 +0800

    1、创建项目； 2、配置podspec文件；

---
+++ HYWebViewDemo/HYWebViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYWebViewDemo
+//
+//  Created by ocean on 2017/9/12.
+//  Copyright © 2017年 ocean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HYWebViewDemo/HYWebViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYWebViewDemo
+//
+//  Created by ocean on 2017/9/12.
+//  Copyright © 2017年 ocean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
